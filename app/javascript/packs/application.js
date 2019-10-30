049b4576e549df83bdfb2fb0b810b915fbd12c31
Wallas Faria <wallasfaria@hotmail.com>
2019-10-29 23:53:56 -0300

    instala startbootstrap sb admin 2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,22 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
+require('popper.js')
+require("bootstrap/dist/js/bootstrap.bundle")
+require("jquery.easing/jquery.easing")
+require('@fortawesome/fontawesome-free/js/all')
+require('imports-loader?define=>false!datatables.net')(window, $)
+require('imports-loader?define=>false!datatables.net-bs4')(window, $)
+require('./shared/sb-admin-2')
+
+// Examples
+require("chartkick")
+require("chart.js")
+
+require('./shared/demo/chart-area-demo')
+require('./shared/demo/chart-bar-demo')
+require('./shared/demo/chart-pie-demo')
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
